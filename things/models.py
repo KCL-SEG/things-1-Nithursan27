373b320771997f5b3cb5cdfb17f57e91eca7c708
Nithursan <nithursan.muraleetharan@kcl.ac.uk>
2023-10-05 18:05:55 +0100

    Add model and refine it

--- things/models.py
+++ things/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Thing(models.Model):
+    name = models.CharField(max_length=30, unique = True, blank = False)
+    description = models.TextField(max_length=120)
+    quantity = models.IntegerField(max_length=100)
